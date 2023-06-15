1acfc81599481ba99b384c1aac678b58b80ad266
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-15 15:14:19 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a text using printf function
+ * Return: 0 (True)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
