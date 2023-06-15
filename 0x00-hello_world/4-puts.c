a44138acb19ce5ec50df74cc78c80189f49a0029
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-15 14:43:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A C program that prints a text using puts function
+ * Return: 0 (True)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
