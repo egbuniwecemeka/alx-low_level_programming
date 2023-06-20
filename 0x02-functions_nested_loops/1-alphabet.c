192ddb28febd2ea22791e2d1a73fe08329a6bdec
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-20 15:43:45 -0700

    Lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints lowercase alphabets.
+ */
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+
+		_putchar(letters);
+
+	_putchar("\n");
+}
