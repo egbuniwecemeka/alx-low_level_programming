1e3ed044327b535a6a107c7d2dc9ef17df90b898
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-23 08:33:18 -0700

    0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,11 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  *
- * Return: 0 (Success)
+ * Return: void
  */
 void print_numbers(void);
 {
-	int x;
+	char x;
 
 	for (x = '0'; x <= '9'; x++)
 	{
