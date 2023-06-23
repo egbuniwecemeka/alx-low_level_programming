04734d73269a7803624eb76b08c9a792beb0b646
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-22 17:10:03 -0700

    isUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+/**
+ * isupper - checks for uppercase character
+ *
+ * Return: 1 for uppercase, zero for others
+ */
+
+int _isupper(int c)
+{
+	int c;
+	int upper;
+
+	if (c >= 65 && c <= 90)
+	{
+		upper = 1;
+	}
+	else
+	{
+		upper = 0;
+	}
+	return (0)
+}
