3647fe9414d080ac4ae5f13b3d2c7c74d11b8717
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-26 15:06:08 -0700

    Updating values with pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 /**
  * reset_to_98 - points to an int and updates the value it points to
  * @n: parameter to update.
- * Return: void
+ * Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
