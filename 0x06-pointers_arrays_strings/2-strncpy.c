d6376aedeecc98a225ca906e8b452b535faf968a
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-29 16:22:03 -0700

    Copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,26 @@
 #include "main.h"
 /**
- * _strncpy - Copies a string
- * using at most n bytes from src
- * @dest: Input string
- * @scr: Input string
- * n: Input value
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
  * Return: dest
  */
-char *_strncpy(char *dest, char *scr, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
 	j = 0;
-
-	while (j < n && scr[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
-		dest[j] = scr[j];
-
+		dest[j] = src[j];
 		j++;
 	}
-
 	while (j < n)
 	{
 		dest[j] = '\0';
+		j++;
 	}
 
 	return (dest);
