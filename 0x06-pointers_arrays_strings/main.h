47586ceb186d58142c7e6d8c4aa2d9ece7b1ee86
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-29 17:14:49 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 char *_strcat(char *dest, char *scr);
 char *_strncat(char *dest, char *scr, int n);
 char *_strncpy(char *dest, char *scr, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
