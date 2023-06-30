3b9ad68ad3faff11833c7abbbb62d9f2b9762276
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-06-30 08:32:11 -0700

    Reverse a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array
+ * @a: pointer to array
+ * @n: number of elements in array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+	
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strcat(char *dest, char *scr);
 char *_strncat(char *dest, char *scr, int n);
 char *_strncpy(char *dest, char *scr, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif
