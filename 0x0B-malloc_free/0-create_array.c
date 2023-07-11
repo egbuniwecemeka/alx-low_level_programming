d5f5a9cf8d08fb70ae6c388fc5ca05c9932d548e
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-07-11 16:07:24 -0700

    betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,9 +6,8 @@
  * @c: char to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
- char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
