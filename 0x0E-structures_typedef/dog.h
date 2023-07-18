13df1459f93c4c58f529657e9944672db16165fc
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-07-18 14:40:27 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - My dog's basic info
+ * @name: Input value
+ * @age: Input value
+ * @owner: Input value
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	/*dog properties*/
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
