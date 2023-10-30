a3d0da25f507a2488d0d8c726407eb3a408a55d4
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-10-30 16:55:21 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/*
+ * create_file - creates s file
+ * @filename: pointer to file
+ * @text-content: text content written i file
+ *
+ * Return: 1 on success, -1 on error
+ */
+
+int create_file(const char *filename, const char *text_content)
+{
+	int fildes;
+	int n;
+	int wr;
+
+	if (!filename)
+		return (-1);
+
+	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fildes == -1)
+		return (-1);
+
+	for (n = 0; text_content[n]; n++);
+
+	if (!text_content)
+		return (-1);
+
+	wr = write(fildes, text_content, n);
+
+	if (wr == -1)
+		return (-1);
+
+	close(fildes);
+
+	return (1);
+}
