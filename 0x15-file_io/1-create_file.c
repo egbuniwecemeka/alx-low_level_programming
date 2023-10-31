863dcd43388b794f2c9a13153998b7011c37490c
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-10-30 17:20:11 -0700

    Create updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (n = 0; text_content[n]; n++);
+	for (n = 0; text_content[n]; n++)
+		;
 
 	wr = write(fildes, text_content, n);
 
