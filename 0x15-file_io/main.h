6e99d436b5b04eea92e97b8b1cc36881bfcb9755
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-10-30 17:03:23 -0700

    Creat

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 int create_file(const char *filename, const char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
