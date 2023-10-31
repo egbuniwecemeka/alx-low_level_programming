0b7c96948038fd75c11334deb6fe11d250e94158
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-10-30 17:07:00 -0700

    Creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * Return: 1 on success, -1 on error
  */
 
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fildes;
 	int n;
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-int create_file(const char *filename, const char *text_content);
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
