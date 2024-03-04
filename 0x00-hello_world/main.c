33290455c2072fceb8b267c7642c89f1a116a5c4
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-04 19:40:45 +0000

    Intel

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Checks everything
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	printf("Holberton School");
 	return (0);
 }
