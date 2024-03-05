6d6784e78c71588d9f923294cb5c232bec35e8e2
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-05 14:45:37 +0000

    print to stderr without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t length = strlen(message);
 	ssize_t bytes_towrite = write(2, message, length);
 	
