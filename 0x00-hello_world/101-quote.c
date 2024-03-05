dea98e055906dc3c1d28a2d22a145042d2cfe771
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-05 14:25:05 +0000

    print to stderr without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - prints a string, and new line to stderr.
+ *
+ * Return: Always 1(Success)
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	ssize_t length = strlen(message);
+	ssize_t bytes_towrite = write(2, message, length);
+	
+	if (bytes_towrite == length)
+	{
+		return (1);
+	}else{
+		return EXIT_FAILURE;
+	}
+}
