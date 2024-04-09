c216a910c829f49006f0fb52b7074457f4b5dd97
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-04-08 19:06:50 -0700

    linear time complexity worst case

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches an array in linear format
+ * @arr: pointer to the first element of the array to search in
+ * @size: lenght of array
+ * @value: value to find
+ *
+ * Return: first index where value is stored
+ */
+int linear_search(int *arr, size_t size, int value)
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (arr[i] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, arr[i]);
+			printf("Value checked array[%d] = [%d]\n", i, arr[i]);
+			return i;
+		}
+
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *arr, size_t size, int value);
+
+#endif
