ebb59b122cdf142b1b0e304ed572f1a6fd9e5d4a
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-01 07:54:28 -0700

    alx deferement

---
+++ 0x00-hello_world/examples/print_initials.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	printf("My initials are %c%c%c. They are %d in no\n", 'E', 'C', 'E', 3);
+	return (0);
+}
---
+++ 0x00-hello_world/examples/scanf_initials.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char f, m, l;
+	int size;
+
+	scanf("%c" "%c" "%c" "%d", &f, &m, &l, &size);
+	printf("My initials are %c%c%c. They are %d in number\n", f, m, l, size);
+	return (0);
+}
