a9fb7cae27471310df525efdcb4d802d9bdcc996
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-26 18:06:25 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,32 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				index = i * j;
-				printf("%d", index);
+				if (index < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(index % 10 + '0');
+				}
+				else if (index < 100)
+				{
+					_putchar(32);
+					_putchar(index / 10 + '0');
+					_putchar(index % 10 + '0');
+				}
+				else
+				{
+					_putchar(index / 100 + '0');
+					_putchar((index / 10) % 10 + '0');
+					_putchar(index % 10 + '0');
+
+				}
 				if (j != n)
 				{
-					printf(", ");
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			printf("\n");
