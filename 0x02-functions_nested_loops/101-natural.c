36b59e7e8add1cf155860e3d30bb39455d05e8a7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-27 14:01:37 -0700

    sum of natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,12 +6,12 @@
  *
  * Return: void
  */
-void sum_natural_numbers(int n)
+int main(void)
 {
 	int i, sum;
 
 	sum = 0;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
