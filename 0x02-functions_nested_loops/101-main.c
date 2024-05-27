d6c3130b592c2091a71f8b44f5acc4f6a24e30e7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-27 08:02:14 -0700

    sum of multiples of n

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	sum_natural_numbers(0);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/** 
+ * sum_natural_numbers - sum multiples of a a natural number
+ * @n: natural number input
+ *
+ * Return: Success (0)
+ */
+void sum_natural_numbers(int n)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
