8b47e7e7a2f029ea8b343b21639d95ded22e6802
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-28 13:14:33 -0700

    sum even finonacci's numbers within a range

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 
-	for (i = 2; i < 50; i++)
+	for (i = 1; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (fibonacci[i] % 2 == 0 && fibonacci[i] < 4000000)
