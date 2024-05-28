b3e1d7deb3d2082e1d73683d44d65e41d5566172
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-28 10:56:24 -0700

    sum of even fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,27 +8,22 @@
 int main(void)
 {
 	int i;
-	long int fibonacci[50];
+	long int fibonacci[50], sum;
+
+	sum = 0;
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
-
-	printf("%ld, %ld", fibonacci[0], fibonacci[1]);
 
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (fibonacci[i] % 2 == 0)
 		{
-			if (i >= 2 && i != 50)
-			{
-				printf(", ");
-			}
-			printf("%ld", fibonacci[i]);
+			sum += fibonacci[i];
 		}
 	}
 
-	printf("\n");
-
+	printf("%ld\n", sum);
 	return (0);
 }
