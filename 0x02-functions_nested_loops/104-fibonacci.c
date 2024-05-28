417c50e5444e9e605819fbb67ebd32f48dbbdf00
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-05-28 13:51:08 -0700

    First 98 finonacci's numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int i;
+
+	long int fibonacci[98];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
+	printf("%ld, %ld", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 98; i++)
+	{
+		if (i > 0 && i != 98)
+		{
+			printf(", ");
+		}
+
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		printf("%ld", fibonacci[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
